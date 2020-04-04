c35b9f860d15683cffd33903ff0d835c6f0e8349
Rickard <rickard.fager@knowit.se>
2020-04-04 16:11:09 +0200

    Added maps

--- app/src/main/java/se/hackforsweden/foodbox/fragments/OrderPositionFragment.kt
+++ app/src/main/java/se/hackforsweden/foodbox/fragments/OrderPositionFragment.kt
@@ -3,13 +3,37 @@ package se.hackforsweden.foodbox.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_order_position.*
 import se.hackforsweden.foodbox.R
 
 class OrderPositionFragment : Fragment(R.layout.fragment_order_position) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            //Todo do stuff when map ready
+        }
     }
 
+    override fun onResume() {
+        mapView.onResume()
+        super.onResume()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
